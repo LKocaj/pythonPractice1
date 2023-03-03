be00b9e538bb064126437fc53126f7b73172b3b3
Lawrence <lawrencecaj@gmail.com>
2023-03-02 20:00:23 -0500

    added notes

--- quiz_game.py
+++ quiz_game.py
@@ -5,4 +5,8 @@ playing = input("Do you want to play? ") #Asking question
 if playing != "yes": # If the answer is anything != but yes, it will quit the program
     quit()
 
-print("Okay! Lets play :)")
+print("Okay! Lets play :)") #If the game doesnt quit - it'll continue on and print this message
+
+answer = input("What does CPU stand for? ") #if You pressed yes then it'll trigger this statement
+if answer == "central processing unit":
+    print('Correct!')
