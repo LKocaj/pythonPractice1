05577e27b27c86ad6278fb40fcf602c25f310830
Lawrence <lawrencecaj@gmail.com>
2023-03-02 19:55:12 -0500

    added print if you say yes

--- quiz_game.py
+++ quiz_game.py
@@ -1,6 +1,8 @@
 print("Welcome to my quiz")
 
-playing = input("Do you want to play? ")
+playing = input("Do you want to play? ") #Asking question
 
-if playing != "yes":
+if playing != "yes": # If the answer is anything != but yes, it will quit the program
     quit()
+
+print("Okay! Lets play :)")
