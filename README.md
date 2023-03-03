ee5c5d68a94a1c9faf76818f5994b2e5cc12e5ff
Lawrence <lawrencecaj@gmail.com>
2023-03-02 20:21:55 -0500

    fixed case sensitive issue with .lower

---
+++ README.md
@@ -0,0 +1,24 @@
+# Python Quiz game
+
+My first ever python "project"
+
+## Description
+
+I ask a bunch of questions to test IF and ELSE statements in the terminal. There are also print commands.
+
+## Getting Started
+
+### Dependencies
+
+* Install python
+
+
+### Installing
+
+* Download folder 
+* open in IDE 
+
+### Executing program
+
+* Run command depending on IDE (press play or run in terminal)
+* Answer all questions correctly!
--- quiz_game.py
+++ quiz_game.py
@@ -5,8 +5,23 @@ playing = input("Do you want to play? ") #Asking question
 if playing != "yes": # If the answer is anything != but yes, it will quit the program
     quit()
 
-print("Okay! Lets play :)") #If the game doesnt quit - it'll continue on and print this message
+print("Okay! Lets play :)") # If the game doesnt quit - it'll continue on and print this message
 
-answer = input("What does CPU stand for? ") #if You pressed yes then it'll trigger this statement
-if answer == "central processing unit":
+answer = input("What does CPU stand for? ") # You answered yes then it'll trigger this statement
+if answer.lower == "central processing unit": # .lower makes anything you type lowercase to match the if statement
     print('Correct!')
+else: 
+    print("Incorrect!")
+
+answer = input("What does RAM stand for? ") # You answered yes then it'll trigger this statement
+if answer.lower == "random access memory":
+    print('Correct!')
+else: 
+    print("Incorrect!")
+
+answer = input("What does PSU stand for? ") 
+if answer.lower == "power supply unit":
+    print('Correct!')
+else: 
+    print("Incorrect!")
+
